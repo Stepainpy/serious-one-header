a78d213f38a5a1f0e05270ce7b8d72167ca4dc99
Stepainpy <stepa2014vlg@gmail.com>
2025-02-13 14:35:19 +0300

    add pjw function

---
+++ hash/pjw.h
@@ -0,0 +1,73 @@
+#ifndef PJW_H
+#define PJW_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+#ifndef PJW_DEF
+#define PJW_DEF
+#endif
+
+PJW_DEF uint32_t pjw_32(const void* source, size_t count);
+PJW_DEF uint64_t pjw_64(const void* source, size_t count);
+
+PJW_DEF uint32_t pjw_32_file(FILE* file);
+PJW_DEF uint64_t pjw_64_file(FILE* file);
+
+#endif // PJW_H
+
+#ifdef PJW_IMPLEMENTATION
+
+uint32_t pjw_32(const void* source, size_t count) {
+    const uint8_t* data = (const uint8_t*)source;
+    uint32_t out = 0, high;
+    while (count --> 0) {
+        out = (out << 4) + *data++;
+        if ((high = 0xF0000000 & out) != 0) {
+            out ^= high >> 24;
+            out &= ~high;
+        }
+    }
+    return out;
+}
+
+uint64_t pjw_64(const void* source, size_t count) {
+    const uint8_t* data = (const uint8_t*)source;
+    uint64_t out = 0, high;
+    while (count --> 0) {
+        out = (out << 8) + *data++;
+        if ((high = 0xFF00000000000000 & out) != 0) {
+            out ^= high >> 48;
+            out &= ~high;
+        }
+    }
+    return out;
+}
+
+uint32_t pjw_32_file(FILE* file) {
+    int ch = 0;
+    uint32_t out = 0, high;
+    while ((ch = fgetc(file)) != EOF) {
+        out = (out << 4) + (uint8_t)ch;
+        if ((high = 0xF0000000 & out) != 0) {
+            out ^= high >> 24;
+            out &= ~high;
+        }
+    }
+    return out;
+}
+
+uint64_t pjw_64_file(FILE* file) {
+    int ch = 0;
+    uint64_t out = 0, high;
+    while ((ch = fgetc(file)) != EOF) {
+        out = (out << 8) + (uint8_t)ch;
+        if ((high = 0xFF00000000000000 & out) != 0) {
+            out ^= high >> 48;
+            out &= ~high;
+        }
+    }
+    return out;
+}
+
+#endif // PJW_IMPLEMENTATION
